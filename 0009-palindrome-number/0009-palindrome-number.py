530a91d77afc5931d82bcbe825f18b827245cd48
Pratyush Kumar <pratyushkumar.pk83@gmail.com>
2026-09-13 16:54:20 +0530

    Time: 2 ms (91.26%), Space: 19.3 MB (54.96%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # Negative numbers and numbers ending in 0 (except 0 itself) are not palindromes
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
 
@@ -9,5 +8,4 @@ class Solution:
             reverted_number = reverted_number * 10 + x % 10
             x //= 10
 
-        # When the length is an odd number, we can get rid of the middle digit by reverted_number // 10
         return x == reverted_number or x == reverted_number // 10
